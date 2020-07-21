a4d8cb9e94200d235d8cca4957dcf5b187818f8f
terauchi <terauchi@ss.cs.osakafu-u.ac.jp>
2020-07-21 13:08:41 +0900

    add Logger and argparse

--- workspace/main.py
+++ workspace/main.py
@@ -1,190 +1,186 @@
-'''Train CIFAR10 with PyTorch.'''
+import fire
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-from torch.optim.lr_scheduler import LambdaLR, CosineAnnealingLR
 
 import torchvision
 import torchvision.transforms as transforms
 from warmup_scheduler import GradualWarmupScheduler
 import os
+import time
+import json
+from pprint import pprint
+import random
 import argparse
+from torch.utils.tensorboard import SummaryWriter
 
-from models import *
-from utils import progress_bar
+from utils import progress_bar, parse_args, get_model_name, select_model, select_optimizer, select_scheduler
 from datetime import datetime
 
-start = datetime.now()
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-parser.add_argument('--resume', '-r', action='store_true',
-                    help='resume from checkpoint')
-args = parser.parse_args()
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-epochs = 200
+def main(**kwargs):
+    start = datetime.now()
+    print('\n[+] Parse arguments')
+    args, kwargs = parse_args(kwargs)
+    pprint(args)
 
-# Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    best_acc = 0  # best test accuracy
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+    print('\n[+] Create log dir')
+    model_name = get_model_name(args)
+    log_dir = os.path.join('./runs', model_name)
+    os.makedirs(os.path.join(log_dir, 'model'))
+    json.dump(kwargs, open(os.path.join(log_dir, 'kwargs.json'), 'w'))
+    writer = SummaryWriter(log_dir=log_dir)
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    print('\n[+] Create network')
+    net = select_model(args)
+    net = net.to(device)
+    optimizer = select_optimizer(args, net)
+    scheduler = select_scheduler(args, optimizer)
+    # originally used scheduler
+    # def func(epoch):
+    #     if epoch < 150:
+    #         return 0.1
+    #     elif epoch < 250:
+    #         return 0.01
+    #     else:
+    #         return 0.001
+    # scheduler = LambdaLR(optimizer, lr_lambda=func)
+    # if args.warmup:
+    #     scheduler = GradualWarmupScheduler(
+    #                 optimizer,
+    #                 multiplier=1,
+    #                 total_epoch=5,
+    #                 after_scheduler=scheduler
+    #             )
+    criterion = nn.CrossEntropyLoss()
+    if args.use_cuda:
+        net = net.cuda()
+        criterion = criterion.cuda()
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+    # Data
+    print('\n[+] Load dataset')
 
-# Model
-print('==> Building model..')
-# net = VGG('VGG19')
-# net = ResNet18()
-# net = PreActResNet18()
-# net = GoogLeNet()
-# net = DenseNet121()
-# net = ResNeXt29_2x64d()
-# net = MobileNet()
-# net = MobileNetV2()
-# net = DPN92()
-# net = ShuffleNetG2()
-# net = SENet18()
-# net = ShuffleNetV2(1)
-# net = EfficientNetB0()
-# net = RegNetX_200MF()
-# net = PyramidNet('cifar10', depth=116, alpha=200, num_classes=10, bottleneck=True)  # originally depth=272
-net = WideResNet(depth=28, widen_factor=10, dropout_rate=0.0, num_classes=10)  # 'wresnet28_2'
-# net = WideResNet(depth=40, widen_factor=2, dropout_rate=0.0, num_classes=10)  # 'wresnet40_2'
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
-net = net.to(device)
-if device == 'cuda':
-    net = torch.nn.DataParallel(net)
-    cudnn.benchmark = True
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
-if args.resume:
-    # Load checkpoint.
-    print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.pth')
-    net.load_state_dict(checkpoint['net'])
-    best_acc = checkpoint['acc']
-    start_epoch = checkpoint['epoch']
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr,
-                      momentum=0.9, weight_decay=5e-4)
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=100, shuffle=False, num_workers=args.num_workers)
 
-# def func(epoch):
-#     if epoch < 150:
-#         return 0.1
-#     elif epoch < 250:
-#         return 0.01
-#     else:
-#         return 0.001
+    classes = ('plane', 'car', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck')
 
+    if device == 'cuda':
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
 
-# scheduler = LambdaLR(optimizer, lr_lambda=func)
-scheduler = CosineAnnealingLR(optimizer, T_max=epochs, eta_min=0.)
-warmup = True
-# if warmup:
-#     scheduler = GradualWarmupScheduler(
-#                 optimizer,
-#                 multiplier=1,
-#                 total_epoch=5,
-#                 after_scheduler=scheduler
-#             )
+    # Training
+    def train(epoch):
+        print('\nEpoch: %d' % epoch)
+        net.train()
+        train_loss = 0
+        correct = 0
+        total = 0
+        total_steps = len(trainloader)
+        steps = 0
 
-
-# Training
-def train(epoch):
-    print('\nEpoch: %d' % epoch)
-    net.train()
-    train_loss = 0
-    correct = 0
-    total = 0
-    total_steps = len(trainloader)
-    steps = 0
-
-    for batch_idx, (inputs, targets) in enumerate(trainloader):
-        steps += 1
-        inputs, targets = inputs.to(device), targets.to(device)
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-
-        train_loss += loss.item()
-        _, predicted = outputs.max(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
-
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
-
-        scheduler.step(epoch - 1 + float(steps) / total_steps)
-        print(scheduler.get_lr())
-
-
-def test(epoch):
-    global best_acc
-    net.eval()
-    test_loss = 0
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(testloader):
+        for batch_idx, (inputs, targets) in enumerate(trainloader):
             inputs, targets = inputs.to(device), targets.to(device)
+            optimizer.zero_grad()
             outputs = net(inputs)
             loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
 
-            test_loss += loss.item()
+            train_loss += loss.item()
             _, predicted = outputs.max(1)
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
-    # Save checkpoint.
-    acc = 100.*correct/total
-    if acc > best_acc:
-        print('Saving..')
-        state = {
-            'net': net.state_dict(),
-            'acc': acc,
-            'epoch': epoch,
-        }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt.pth')
-        best_acc = acc
+            scheduler.step(epoch - 1 + float(batch_idx+1) / total_steps)
+            print(scheduler.get_lr())
+
+        writer.add_scalar('train/learning_rate', optimizer.param_groups[0]['lr'], global_step=epoch)
+        writer.add_scalar('train/acc1', correct / total, global_step=epoch)
+        writer.add_scalar('train/loss', train_loss / (batch_idx+1), global_step=epoch)
+
+    def test(epoch):
+        nonlocal best_acc
+        net.eval()
+        test_loss = 0
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for batch_idx, (inputs, targets) in enumerate(testloader):
+                inputs, targets = inputs.to(device), targets.to(device)
+                outputs = net(inputs)
+                loss = criterion(outputs, targets)
+
+                test_loss += loss.item()
+                _, predicted = outputs.max(1)
+                total += targets.size(0)
+                correct += predicted.eq(targets).sum().item()
+
+                progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                             % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+        writer.add_scalar('valid/acc1', correct/total, global_step=epoch)
+        writer.add_scalar('valid/loss', test_loss/(batch_idx+1), global_step=epoch)
+
+        # Save checkpoint.
+        acc = 100.*correct/total
+        if acc > best_acc:
+            best_acc = acc
+            print('Saving..')
+            state = {
+                'net': net.state_dict(),
+                'acc': acc,
+                'epoch': epoch,
+            }
+            torch.save(state, os.path.join(log_dir, "model", "model.pt"))
+
+    print('\n[+] Start training')
+    if torch.cuda.device_count() > 1:
+        print('\n[+] Use {} GPUs'.format(torch.cuda.device_count()))
+        net = nn.DataParallel(net)
+
+    for epoch in range(args.epochs):
+        train(epoch)
+        test(epoch)
+    print("Train Started at {}".format(start))
+    finish = datetime.now()
+    print("Train Finished at {}".format(finish))
+    print("Elapsed Time: {}".format(finish - start))
+    print("best_accuracy:", best_acc)
 
 
-for epoch in range(epochs):
-    train(epoch)
-    test(epoch)
-print("Train Started at {}".format(start))
-finish = datetime.now()
-print("Train Finished at {}".format(finish))
-print("Elapsed Time: {}".format(finish - start))
-print("best_accuracy:", best_acc)
+if __name__ == "__main__":
+    fire.Fire(main())
--- workspace/utils.py
+++ workspace/utils.py
@@ -7,9 +7,11 @@ import os
 import sys
 import time
 import math
-
+import torch
 import torch.nn as nn
 import torch.nn.init as init
+import collections
+from models import *
 
 
 def get_mean_and_std(dataset):
@@ -20,11 +22,12 @@ def get_mean_and_std(dataset):
     print('==> Computing mean and std..')
     for inputs, targets in dataloader:
         for i in range(3):
-            mean[i] += inputs[:,i,:,:].mean()
-            std[i] += inputs[:,i,:,:].std()
+            mean[i] += inputs[:, i, :, :].mean()
+            std[i] += inputs[:, i, :, :].std()
     mean.div_(len(dataset))
     std.div_(len(dataset))
     return mean, std
+
 
 def init_params(net):
     '''Init layer parameters.'''
@@ -48,12 +51,14 @@ term_width = int(term_width)
 TOTAL_BAR_LENGTH = 65.
 last_time = time.time()
 begin_time = last_time
+
+
 def progress_bar(current, total, msg=None):
     global last_time, begin_time
     if current == 0:
         begin_time = time.time()  # Reset for new bar.
 
-    cur_len = int(TOTAL_BAR_LENGTH*current/total)
+    cur_len = int(TOTAL_BAR_LENGTH * current / total)
     rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
 
     sys.stdout.write(' [')
@@ -77,30 +82,31 @@ def progress_bar(current, total, msg=None):
 
     msg = ''.join(L)
     sys.stdout.write(msg)
-    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
+    for i in range(term_width - int(TOTAL_BAR_LENGTH) - len(msg) - 3):
         sys.stdout.write(' ')
 
     # Go back to the center of the bar.
-    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
+    for i in range(term_width - int(TOTAL_BAR_LENGTH / 2) + 2):
         sys.stdout.write('\b')
-    sys.stdout.write(' %d/%d ' % (current+1, total))
+    sys.stdout.write(' %d/%d ' % (current + 1, total))
 
-    if current < total-1:
+    if current < total - 1:
         sys.stdout.write('\r')
     else:
         sys.stdout.write('\n')
     sys.stdout.flush()
 
+
 def format_time(seconds):
-    days = int(seconds / 3600/24)
-    seconds = seconds - days*3600*24
+    days = int(seconds / 3600 / 24)
+    seconds = seconds - days * 3600 * 24
     hours = int(seconds / 3600)
-    seconds = seconds - hours*3600
+    seconds = seconds - hours * 3600
     minutes = int(seconds / 60)
-    seconds = seconds - minutes*60
+    seconds = seconds - minutes * 60
     secondsf = int(seconds)
     seconds = seconds - secondsf
-    millis = int(seconds*1000)
+    millis = int(seconds * 1000)
 
     f = ''
     i = 1
@@ -122,3 +128,117 @@ def format_time(seconds):
     if f == '':
         f = '0ms'
     return f
+
+
+def dict_to_namedtuple(d):
+    Args = collections.namedtuple('Args', sorted(d.keys()))
+
+    for k, v in d.items():
+        if type(v) is dict:
+            d[k] = dict_to_namedtuple(v)
+
+        elif type(v) is str:
+            try:
+                d[k] = eval(v)
+            except:
+                d[k] = v
+
+    args = Args(**d)
+    return args
+
+
+def parse_args(kwargs):
+    # combine with default args
+    kwargs['dataset'] = kwargs['dataset'] if 'dataset' in kwargs else 'cifar10'
+    kwargs['num_classes'] = kwargs['num_classes'] if 'num_classes' in kwargs else 10
+    kwargs['network'] = kwargs['network'] if 'network' in kwargs else 'wresnet28_2'
+    kwargs['optimizer'] = kwargs['optimizer'] if 'optimizer' in kwargs else 'sgd'
+    kwargs['lr'] = kwargs['lr'] if 'lr' in kwargs else 0.1
+    kwargs['seed'] = kwargs['seed'] if 'seed' in kwargs else None
+    kwargs['use_cuda'] = kwargs['use_cuda'] if 'use_cuda' in kwargs else True
+    kwargs['use_cuda'] = kwargs['use_cuda'] and torch.cuda.is_available()
+    kwargs['num_workers'] = kwargs['num_workers'] if 'num_workers' in kwargs else 4
+    kwargs['print_step'] = kwargs['print_step'] if 'print_step' in kwargs else 2000
+    kwargs['val_step'] = kwargs['val_step'] if 'val_step' in kwargs else 2000
+    kwargs['scheduler'] = kwargs['scheduler'] if 'scheduler' in kwargs else 'cosine'
+    kwargs['batch_size'] = kwargs['batch_size'] if 'batch_size' in kwargs else 128
+    kwargs['start_step'] = kwargs['start_step'] if 'start_step' in kwargs else 0
+    kwargs['max_step'] = kwargs['max_step'] if 'max_step' in kwargs else 64000
+    kwargs['epochs'] = kwargs['epochs'] if 'epochs' in kwargs else 200
+    kwargs['warmup'] = kwargs['warmup'] if 'warmup' in kwargs else False
+    kwargs['auto_augment'] = kwargs['auto_augment'] if 'auto_augment' in kwargs else False
+    kwargs['augment_path'] = kwargs['augment_path'] if 'augment_path' in kwargs else None
+    kwargs['mag'] = kwargs['mag'] if 'mag' in kwargs else None
+    kwargs['tinit'] = kwargs['tinit'] if 'tinit' in kwargs else None
+    kwargs['tfin'] = kwargs['tfin'] if 'tfin' in kwargs else None
+
+    # to named tuple
+    args = dict_to_namedtuple(kwargs)
+    return args, kwargs
+
+
+def get_model_name(args):
+    from datetime import datetime
+    now = datetime.now()
+    date_time = now.strftime("%B_%d_%H:%M:%S")
+    model_name = '__'.join([date_time, args.network, str(args.seed)])
+    return model_name
+
+
+def select_model(args):
+    if args.network == "wresnet28_2":
+        net = WideResNet(depth=28, widen_factor=2, dropout_rate=0.0, num_classes=args.num_classes)
+    elif args.network == "wresnet28_10":
+        net = WideResNet(depth=28, widen_factor=10, dropout_rate=0.0, num_classes=args.num_classes)
+    elif args.network == "RegNetX_200MF":
+        net = RegNetX_200MF()
+    else:  # cifer10以外のデータセットを使う場合はクラス数の指定に注意
+        net = WideResNet(depth=28, widen_factor=2, dropout_rate=0.0, num_classes=args.num_classes)
+
+    # net = VGG('VGG19')
+    # net = ResNet18()
+    # net = PreActResNet18()
+    # net = GoogLeNet()
+    # net = DenseNet121()
+    # net = ResNeXt29_2x64d()
+    # net = MobileNet()
+    # net = MobileNetV2()
+    # net = DPN92()
+    # net = ShuffleNetG2()
+    # net = SENet18()
+    # net = ShuffleNetV2(1)
+    # net = EfficientNetB0()
+    # net = RegNetX_200MF()
+    # net = PyramidNet('cifar10', depth=116, alpha=200, num_classes=10, bottleneck=True)  # originally depth=272
+    # net = WideResNet(depth=28, widen_factor=2, dropout_rate=0.0, num_classes=10)  # 'wresnet28_2or10'
+    # net = WideResNet(depth=40, widen_factor=2, dropout_rate=0.0, num_classes=10)  # 'wresnet40_2'
+
+    print(net)
+    return net
+
+
+def select_optimizer(args, net):
+    if args.optimizer == 'sgd':
+        optimizer = torch.optim.SGD(net.parameters(), lr=args.lr,
+                              momentum=0.9, weight_decay=5e-4)
+    elif args.optimizer == 'rms':
+        #optimizer = torch.optim.RMSprop(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=1e-5)
+        optimizer = torch.optim.RMSprop(net.parameters(), lr=args.lr)
+    elif args.optimizer == 'adam':
+        optimizer = torch.optim.Adam(net.parameters(), lr=args.lr)
+    else:
+        raise Exception('Unknown Optimizer')
+    return optimizer
+
+
+def select_scheduler(args, optimizer):
+    if not args.scheduler or args.scheduler == 'None':
+        return None
+    elif args.scheduler =='clr':
+        return torch.optim.lr_scheduler.CyclicLR(optimizer, 0.01, 0.015, mode='triangular2', step_size_up=250000, cycle_momentum=False)
+    elif args.scheduler =='exp':
+        return torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9999283, last_epoch=-1)
+    elif args.scheduler == "cosine":
+        return torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs, eta_min=0.)
+    else:
+        raise Exception('Unknown Scheduler')
